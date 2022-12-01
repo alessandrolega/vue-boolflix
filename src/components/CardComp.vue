569d8b898501e981acf2f13bc757cd159cf435a8
alessandrolega <alessandrolega24@gmail.com>
2022-12-01 12:44:39 +0100

    aggiunte flags

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="card" style="width: 18rem;">
+
         <img :src="`http://image.tmdb.org/t/p/w342/${singleFilm.poster_path}`" class="card-img-top" alt="poster">
         <div class="card-body">
             <h5 class="card-title">{{singleFilm.title}}</h5>
             <ul>
                 <li>{{singleFilm.original_title}}</li>
-                <li>{{singleFilm.original_language}}</li>
+                <li><img class="flag" :src=" `https://www.countryflagicons.com/SHINY/64/${ (singleFilm.original_language == 'en') ? 'GB' : ((singleFilm.original_language == 'ja') ? 'JP' : singleFilm.original_language.toUpperCase())}.png`"></li>
                 <li>{{singleFilm.vote_average}}</li>
+
+                 
+
             </ul>
         </div>
     </div>
@@ -40,13 +44,17 @@
     color: white;
 }
 
-.card:hover img{
+.card:hover > img{
     display: none;
 
 }
 
 .card:hover .card-body{
     display: block;
+}
+
+.flag{
+    width: 30px;
 }
 
 
